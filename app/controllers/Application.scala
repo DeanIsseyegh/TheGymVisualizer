da1419a2e1796ab8933d5625f907b63affe8f769
DeanIsseyegh <issdea@gmail.com>
2015-05-26 08:30:01 +0100

    Added exception handling for bad connection to TheGym

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,41 +10,44 @@ import models.UserData
 import play.api.libs.json.Json
 
 object Application extends Controller {
-	def index = Action {
-		Ok(views.html.index())
-	}
-  
+  def index = Action {
+    Ok(views.html.index(""))
+  }
+
   val form = Form(
     tuple(
       "email" -> text,
-      "pin" -> number
-    )
-  )
-  
+      "pin" -> number))
+
   def submit = Action { implicit request =>
     println("Submit called")
+    try {
     val (email, pin) = form.bindFromRequest.get
     val gymUsage = retrieveGymUsage(email, pin.toString())
     val userData = mapToUserDataObject(gymUsage)
-    //Ok("Hi %s, your pin is %s.".format(email, pin))
     Ok(views.html.gymusage(userData))
+    } catch {
+      case _ : Throwable => { println("Uh oh! Could not scrape")
+        Ok(views.html.index("Oops! Could not connect to TheGym - please ensure you entered the correct email/pin.")) }
+    }
   }
-  
-  def retrieveGymUsage(user:String, pin:String):GymUsageData = {
+
+  def retrieveGymUsage(user: String, pin: String): GymUsageData = {
     val soup = new SoupScrape(user, pin)
     val gymUsage = new GymUsageData(soup)
     gymUsage
+
   }
-  
-  def mapToUserDataObject(gymUsageData:GymUsageData):UserData = {
+
+  def mapToUserDataObject(gymUsageData: GymUsageData): UserData = {
     val userData = new UserData(gymUsageData.getTotalNumOfSessions,
-        gymUsageData.getAverageNumOfSessionsPerWeek,
-        gymUsageData.getAverageNumOfSessionsPerMonth,
-        gymUsageData.getAverageSessionLengthMins,
-        gymUsageData.getTotalSessionLengthMins,
-        gymUsageData.getDaysSinceFirstAndLastSession,
-        gymUsageData.getDaysSinceFirstSessionAndNow)
+      gymUsageData.getAverageNumOfSessionsPerWeek,
+      gymUsageData.getAverageNumOfSessionsPerMonth,
+      gymUsageData.getAverageSessionLengthMins,
+      gymUsageData.getTotalSessionLengthMins,
+      gymUsageData.getDaysSinceFirstAndLastSession,
+      gymUsageData.getDaysSinceFirstSessionAndNow)
     userData
   }
-  
+
 }
